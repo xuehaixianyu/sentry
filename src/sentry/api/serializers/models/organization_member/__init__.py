5b01b321e45efdf4374a14f6f28cdbedb7c6fac2
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-02-23 16:03:11 -0800

    Move endpoint to module

--- src/sentry/api/serializers/models/organization_member/__init__.py
+++ src/sentry/api/serializers/models/organization_member/__init__.py
@@ -2,7 +2,8 @@ from .base import OrganizationMemberSerializer
 from .expand.projects import OrganizationMemberWithProjectsSerializer
 from .expand.teams import OrganizationMemberWithTeamsSerializer
 from .response import OrganizationMemberSCIMSerializerResponse, SCIMMeta
-from .scim import OrganizationMemberSCIMSerializer
+from .response import SCIMMeta
+from .scim import OrganizationMemberSCIMSerializer, OrganizationMemberSCIMSerializerResponse
 
 __all__ = (
     "OrganizationMemberSCIMSerializer",
